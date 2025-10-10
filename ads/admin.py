f218dfa431829579b93e075140817487df384238
msakr99 <pharmasky4601@gmail.com>
2025-10-10 19:19:06 +0300

    fix: Remove created_at from ads admin (field doesn't exist)

--- ads/admin.py
+++ ads/admin.py
@@ -6,8 +6,7 @@ from ads.models import Advertisment
 
 @admin.register(Advertisment)
 class AdvertismentModelAdmin(DefaultBaseAdminItems):
-    list_display = ("image", "redirect_type", "content_type", "object_id", "external_url", "created_at")
-    list_filter = ("redirect_type", "created_at")
+    list_display = ("image", "redirect_type", "content_type", "object_id", "external_url")
+    list_filter = ("redirect_type",)
     list_select_related = ("content_type",)
     search_fields = ("external_url",)
-    date_hierarchy = "created_at"
